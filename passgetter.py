ce50b345698c15b9ef31d396acdf45ffaa551232
Dobeq <iamwillfischer@gmail.com>
2018-10-26 20:53:49 -0400

    made the path easier to understand

--- passgetter.py
+++ passgetter.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 def getCreds():
-    with open(input("enter the league path: "), 'r+') as file:
+    with open(input("enter the league path: " + "/lockfile"), 'r+') as file:
         text = file.read()
         port = text[19:24]
         pw = text[25:47]
